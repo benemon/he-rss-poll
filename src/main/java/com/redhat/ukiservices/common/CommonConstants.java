8a0e9a7e6c3fcdbbbe3caf75712f5f888331676a
Benjamin Holmes <benjamin.holmes@gmail.com>
2017-07-07 19:34:59 +0100

    Updated to read from multiple sources

--- src/main/java/com/redhat/ukiservices/common/CommonConstants.java
+++ src/main/java/com/redhat/ukiservices/common/CommonConstants.java
@@ -6,11 +6,11 @@ public interface CommonConstants {
 
 	public static final String NOT_OK = "OK";
 
-	public static final String HE_RSS_URL_ENV = "HE_RSS_URL";
+	public static final String HE_RSS_URL_LIST_ENV = "HE_RSS_URL_LIST";
 	public static final String HE_RSS_URL_DEFAULT = "http://m.highways.gov.uk/feeds/rss/UnplannedEvents.xml";
 
 	public static final String POLL_PERIOD_ENV = "HE_RSS_POLL_PERIOD";
-	public static final String POLL_PERIOD_DEFAULT = "10000";
+	public static final String POLL_PERIOD_DEFAULT = "60000";
 
 	public static final String VERTX_EVENT_BUS_HE_RSS_PROCESS = "he.rss.process";
 
--- src/main/java/com/redhat/ukiservices/poll/PollingVerticle.java
+++ src/main/java/com/redhat/ukiservices/poll/PollingVerticle.java
@@ -1,20 +1,34 @@
 package com.redhat.ukiservices.poll;
 
 import java.io.IOException;
+import java.io.StringReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.jdom2.Document;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
+import org.xml.sax.InputSource;
 
 import com.redhat.ukiservices.common.CommonConstants;
 import com.redhat.ukiservices.utils.RssUtils;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.CompositeFuture;
 import io.vertx.core.Context;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
@@ -24,51 +38,118 @@ public class PollingVerticle extends AbstractVerticle {
 
 	private static final Logger log = LoggerFactory.getLogger("PollingVerticle");
 
+	private Long timerId;
 	private Long pollPeriod;
 
-	private String heRssUrl;
+	private String heRssUrls;
+
+	private HttpClient httpClient;
 
 	@Override
 	public void init(Vertx vertx, Context context) {
 		super.init(vertx, context);
 
-		heRssUrl = System.getenv(CommonConstants.HE_RSS_URL_ENV) != null ? System.getenv(CommonConstants.HE_RSS_URL_ENV)
-				: CommonConstants.HE_RSS_URL_DEFAULT;
+		heRssUrls = System.getenv(CommonConstants.HE_RSS_URL_LIST_ENV) != null
+				? System.getenv(CommonConstants.HE_RSS_URL_LIST_ENV) : CommonConstants.HE_RSS_URL_DEFAULT;
 
 		pollPeriod = Long.parseLong(System.getenv(CommonConstants.POLL_PERIOD_ENV) != null
 				? System.getenv(CommonConstants.POLL_PERIOD_ENV) : CommonConstants.POLL_PERIOD_DEFAULT);
+
+		httpClient = vertx.createHttpClient();
+
 	}
 
 	@Override
-	public void start() throws Exception {
+	public void start(Future<Void> future) throws Exception {
 		super.start();
 
-		vertx.setPeriodic(pollPeriod, timerId -> fetchRss());
+		parseRssTargets();
+
+		future.complete();
+
 	}
 
-	private Future<Void> fetchRss() {
+	@Override
+	public void stop(Future<Void> future) {
+		if (timerId != null) {
+			vertx.cancelTimer(timerId);
+		}
+
+		if (httpClient != null) {
+			httpClient.close();
+		}
+
+		future.complete();
+	}
+
+	private void parseRssTargets() {
+		List<String> urls = Arrays.asList(heRssUrls.split(","));
+
+		readRssFeedDetails(urls);
+
+	}
+
+	private void readRssFeedDetails(List<String> urls) {
+
+		CompositeFuture.all(urls.stream().map(this::readFeed).collect(Collectors.toList()))
+				.setHandler(fetchResult -> timerId = vertx.setTimer(pollPeriod, timerIdentifier -> parseRssTargets()));
+
+	}
+
+	private Future<Void> readFeed(String feedUrl) {
 		Future<Void> future = Future.future();
+		long start = System.currentTimeMillis();
 
-		log.info("Start RSS Fetch...");
+		log.info("Reading RSS Feed: " + feedUrl);
 
-		createDocument(future);
+		URL url;
+
+		try {
+			url = new URL(feedUrl);
+		} catch (MalformedURLException mfe) {
+			log.warn("Invalid url : " + feedUrl, mfe);
+			return Future.failedFuture(mfe);
+		}
+
+		this.getXml(url, response -> {
+			int status = response.statusCode();
+			if (status < 200 || status >= 300) {
+				if (future != null) {
+					future.fail(new RuntimeException(
+							"Could not read feed " + feedUrl + ". Response status code : " + status));
+				}
+				return;
+			}
+			response.bodyHandler(buffer -> this.parseXml(buffer, future));
+		});
+
+		long stop = System.currentTimeMillis();
+
+		log.info("Read completed in " + (stop - start) + "ms");
 
 		return future;
 	}
 
-	private void createDocument(Future<Void> future) {
+	private void getXml(URL url, Handler<HttpClientResponse> responseHandler) {
+
+		httpClient.get(url.getHost(), url.getPath(), responseHandler).putHeader(HttpHeaders.ACCEPT, "application/xml")
+				.end();
+	}
+
+	private void parseXml(Buffer buffer, Future<Void> future) {
+		String xmlFeed = buffer.toString("UTF-8");
+
 		final SAXBuilder sax = new SAXBuilder();
+
 		try {
-			Document doc = sax.build(new URL(heRssUrl));
-			
+
+			Document doc = sax.build(new InputSource(new StringReader(xmlFeed)));
 			List<JsonObject> entries = RssUtils.toJson(doc);
-
 			vertx.eventBus().publish((CommonConstants.VERTX_EVENT_BUS_HE_RSS_PROCESS), new JsonArray(entries));
-
 			future.complete();
 
 		} catch (JDOMException | IOException e) {
-			future.fail("Exception caught when building SAX Document");
+			future.fail(new RuntimeException("Exception caught when building SAX Document", e));
 		}
 	}
 
